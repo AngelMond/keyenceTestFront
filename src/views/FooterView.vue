bdc59675579c6ea3d13fb1ec63dee64425433140
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-03 01:48:39 -0600

    Footer created

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,5 +1,34 @@
 <template>
-    <footer>
-        <p class="text-red">my footer</p>
-    </footer>
-</template>
+    <v-footer class="bg-grey-lighten-1">
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Keyence</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
+  }),
+}
+</script>
