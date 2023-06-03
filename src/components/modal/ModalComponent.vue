2403f7087f8d3ddd6da8d1d302feab849658fc38
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-03 16:36:53 -0600

    created modal component

---
+++ src/components/modal/ModalComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-dialog v-model="dialogVisible" max-width="500px">
+        <v-card>
+            <v-card-title>
+                <span class="headline">{{ modalTitle }}</span>
+            </v-card-title>
+            <v-card-text>
+                <p>{{ modalBody }}</p>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="closeDialog">Aceptar</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+  
+<script>
+export default {
+    props: {
+        modalTitle: {
+            type: String,
+            default: "Moda title"
+        },
+        modalBody: {
+            type: String,
+            default: "MOdal body"
+        }
+    },
+    data() {
+        return {
+            dialogVisible: true,
+        };
+    },
+    methods: {
+        openDialog() {
+            this.dialogVisible = true;
+        },
+        closeDialog() {
+            this.dialogVisible = false;
+        }
+    }
+};
+</script>
+  
