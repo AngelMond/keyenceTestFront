91dd2f45d708d0c884cf3b7320f566030eeca758
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-03 18:25:57 -0600

    NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        <h1>Page not found</h1>
+    <div class="container d-flex align-center">
+        <v-row justify="center"  >
+            <v-col cols="12" class="justify-center">
+                <h1 class="text-red text-center text-align-center">Page not found</h1>
+            </v-col>
+        </v-row>
     </div>
 </template>
